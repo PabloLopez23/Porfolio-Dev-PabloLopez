9e574b16decdbd4951ae7fcd14b7e03bb0fafd56
PabloLopez23 <pabloexelopezh@gmail.com>
2024-11-23 16:35:48 -0300

    level11

--- src/data/projects.mock.ts
+++ src/data/projects.mock.ts
@@ -13,21 +13,21 @@ export const mockProjects: Project[] = [
     title: 'BEST MOVIE TRAILERS',
     description: 'Aplicación Web de trailers de películas desarrollada con React, donde puedes explorar y consultar información de trailers de películas más actuales y populares. Utilizando la API externa de Themoviedb.',
     skills: ["TypeScript","React","bootstrap","git","Axios", "Api", "Node.js"],
-    urlImage: 'https://drive.google.com/file/d/182NtT9o9VC3Kfk-kCvNTz0RcfSginvdu/view?usp=sharing', 
+    urlImage: 'https://res.cloudinary.com/dxakaoudm/image/upload/v1732389918/img1_deiska.jpg', 
     urlRepository: 'https://github.com/PabloLopez23/Best-Movie-Trailers',
     urlDeploy: 'https://moonlit-syrniki-83ced5.netlify.app/'
   },
   {
     title: 'BARBERHUB',
     description: 'En colaboración con compañeros de todo el mundo, participé en el desarrollo de una aplicación Web para una barbería. Nuestro objetivo fue crear una plataforma intuitiva y funcional que permitiera a los usuarios reservar citas, ver los servicios disponibles y obtener información sobre los barberos.',
-    urlImage: '/public/img3.png', 
+    urlImage: 'https://res.cloudinary.com/dxakaoudm/image/upload/v1732390027/img2_ghgatr.jpg', 
     skills: ["Node.js","TypeScript","React","Redux","TailwindCSS",],
     urlRepository: 'https://github.com/Heba-WebDev/barbershop',
     urlDeploy: 'https://drive.google.com/file/d/1H1S3dTU0ZS_Fl6zovThnI_87PoGG_NbX/view?usp=sharing',
   },
   {
     title: 'SEARCHER GITHUB',
-    urlImage: '/public/46.jpeg',
+    urlImage: 'https://res.cloudinary.com/dxakaoudm/image/upload/v1732390061/img3_k0t9sc.jpg',
     description: 'Aplicacion realizada con React, es un buscador de usuarios de GitHub y muestra la informacion mas relevante como cantidad de Repositorios, Seguidores, etc.',
     skills: ["React","TypeScript","CSS","Axios","Api",],
     urlRepository: 'https://github.com/PabloLopez23/Searcher_GitHub',
@@ -35,10 +35,10 @@ export const mockProjects: Project[] = [
   },
   {
     title: 'PORFOLIO DEV',
-    urlImage: '/public/28.jpeg',
+    urlImage: 'https://res.cloudinary.com/dxakaoudm/image/upload/v1732390073/img4_femr3y.jpg',
     description: 'Aplicación de portafolio usando Astro, JavaScript, TypeScript, TailwindCSS, etc. La aplicación es responsiva, incluye una sección de proyectos con detalles y enlaces.',
     skills: ["Astro","Javascript","TypeScript","TailwindCSS","Axios","Node.js",],
     urlRepository: 'https://github.com/PabloLopez23/Porfolio-Dev-PabloLopez',
-    urlDeploy: 'https://github.com/PabloLopez23/Porfolio-Dev-PabloLopez',
+    urlDeploy: 'https://teal-genie-00ce0e.netlify.app/',
   }
 ]
