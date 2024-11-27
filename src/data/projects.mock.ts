4fccf0900579552998e0ba22e6d4b876e9a670eb
PabloLopez23 <pabloexelopezh@gmail.com>
2024-11-26 23:58:37 -0300

    level22

--- src/data/projects.mock.ts
+++ src/data/projects.mock.ts
@@ -15,15 +15,7 @@ export const mockProjects: Project[] = [
     skills: ["TypeScript","React","bootstrap","git","Axios", "Api", "Node.js","Responsive Web Design"],
     urlImage: 'https://res.cloudinary.com/dxakaoudm/image/upload/v1732389918/img1_deiska.jpg', 
     urlRepository: 'https://github.com/PabloLopez23/Best-Movie-Trailers',
-    urlDeploy: 'https://moonlit-syrniki-83ced5.netlify.app/'
-  },
-  {
-    title: 'BARBERHUB',
-    description: 'En colaboración con compañeros de todo el mundo, participé en el desarrollo de una aplicación Web para una barbería. Nuestro objetivo fue crear una plataforma intuitiva y funcional que permitiera a los usuarios reservar citas, ver los servicios disponibles y obtener información sobre los barberos.',
-    urlImage: 'https://res.cloudinary.com/dxakaoudm/image/upload/v1732390027/img2_ghgatr.jpg', 
-    skills: ["Node.js","TypeScript","React","Redux","TailwindCSS","Responsive Web Design"],
-    urlRepository: 'https://github.com/Heba-WebDev/barbershop',
-    urlDeploy: 'https://drive.google.com/file/d/1H1S3dTU0ZS_Fl6zovThnI_87PoGG_NbX/view?usp=sharing',
+    urlDeploy: 'https://pablolopezmovies.netlify.app/'
   },
   {
     title: 'SEARCHER GITHUB',
@@ -39,6 +31,14 @@ export const mockProjects: Project[] = [
     description: 'Aplicación de portafolio usando Astro, JavaScript, TypeScript, TailwindCSS, etc. La aplicación es responsiva, incluye una sección de proyectos con detalles y enlaces.',
     skills: ["Astro","Javascript","TypeScript","TailwindCSS","Axios","Node.js","Responsive Web Design"],
     urlRepository: 'https://github.com/PabloLopez23/Porfolio-Dev-PabloLopez',
-    urlDeploy: 'https://teal-genie-00ce0e.netlify.app/',
-  }
+    urlDeploy: 'https://pablolopezporfolio.netlify.app/',
+  },
+  {
+    title: 'BARBERHUB',
+    description: 'En colaboración con compañeros de todo el mundo, participé en el desarrollo de una aplicación Web para una barbería. Nuestro objetivo fue crear una plataforma intuitiva y funcional que permitiera a los usuarios reservar citas, ver los servicios disponibles y obtener información sobre los barberos.',
+    urlImage: 'https://res.cloudinary.com/dxakaoudm/image/upload/v1732390027/img2_ghgatr.jpg', 
+    skills: ["Node.js","TypeScript","React","Redux","TailwindCSS","Responsive Web Design"],
+    urlRepository: 'https://github.com/Heba-WebDev/barbershop',
+    urlDeploy: 'https://drive.google.com/file/d/1H1S3dTU0ZS_Fl6zovThnI_87PoGG_NbX/view?usp=sharing',
+  },
 ]
