c5e594b25916b40866d9d571f2180108e04fc172
PabloLopez23 <pabloexelopezh@gmail.com>
2025-07-28 02:11:35 -0300

    level25

--- src/data/education.mock.ts
+++ src/data/education.mock.ts
@@ -4,16 +4,16 @@ export const mockEducation: TimeLineData[] = [
   {
     subTitle: { title: "Diploma - Enlace", url: "https://platzi.com/p/pabloexelh22/ruta/100-javascript-full-stack/diploma/detalle/"},
     date: "PLATZI educación profesional online - Junio 2023",
-    title: "Full Stack Developer JavaScript",
+    title: "Desarrollador de software",
   },
   {
     subTitle: { title: "Diploma - Enlace", url: "https://platzi.com/p/pabloexelh22/ruta/38-seguridad-informatica-old/diploma/detalle/"},
     date: "PLATZI educación profesional online - Julio 2023",
-    title: "Cybersecurity",
+    title: "Analista de Ciberseguridad",
   },
   {
     subTitle: { title: "Diploma - Enlace", url: "https://platzi.com/p/pabloexelh22/ruta/12050-beginner-core2/diploma/detalle/"},
     date: "PLATZI educación profesional online - Agosto 2023",
-    title: "English A2",
+    title: "Ingles",
   },
 ];
